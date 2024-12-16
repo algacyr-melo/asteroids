1712953be7007cfa958b7aa7171652dd2d93691b
algacyr <algacyroalga@gmail.com>
2024-12-16 20:10:58 -0300

    destroying some flying asteroids

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import sys
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
+from shot import Shot
 
 
 def main():
@@ -11,13 +15,19 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
-
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    dt = 0
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
 
+    dt = 0
     while True:
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
@@ -27,6 +37,16 @@ def main():
 
         for obj in updatable:
             obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game Over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.kill()
+                    shot.kill()
 
         screen.fill("black")
 
@@ -42,3 +62,4 @@ def main():
 
 if __name__=="__main__":
     main()
+
